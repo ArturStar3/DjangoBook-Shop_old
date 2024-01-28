fc7197420e81bdb62076a2e8c524fdbec39cf9d2
ArturStar3 <artur.chviire@mail.ru>
2024-01-28 19:21:27 +0600

    Shop_FirstModels

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'availabel', 'created', 'updated']
+    list_filter = ['availabel', 'created', 'updated']
+    list_editable = ['price', 'availabel']
+    prepopulated_fields = {'slug':('name',)}
